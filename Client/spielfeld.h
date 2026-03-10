ab1b30d1ccc7fad16e6c3c9e322639d69c1fad0b
JenLorenz <Jennifer.Lorenz@bs-erlangen.de>
2026-03-10 11:04:25 +0100

    Spielfeld
    Spielfeld.h Datei umgezogen.

--- Client/spielfeld.h
+++ Client/spielfeld.h
@@ -3,8 +3,17 @@
 
 class Spielfeld
 {
+private:
+    int spielfeld[10][10];
+    int getroffeneFelder[10][10];
 public:
     Spielfeld();
+    void setSpielfeld(int x, int y, int wert);
+    int getSpielfeld(int x, int y) const;
+    void setGetroffenesFeld(int x, int y);
+    int getGetroffenesFeld(int x, int y) const;
+    bool schuss(int x, int y); // true = Treffer, false = daneben
+    void printSpielfeld(bool sichtbar = false) const;
 };
 
 #endif // SPIELFELD_H
